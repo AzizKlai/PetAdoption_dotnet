187f1ff90b700f49ca7dc7d23ec108dc176022b6
AzizKlai <mohamedaziz.klai@insat.u-carthage.tn>
2022-12-20 09:40:41 +0100

    correct project name

--- obj/Debug/net7.0/PetAdoption_dotnet.AssemblyInfo.cs
+++ obj/Debug/net7.0/PetAdoption_dotnet.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PetAdoption_dotnet_project")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PetAdoption_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("PetAdoption_dotnet_project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PetAdoption_dotnet_project")]
+[assembly: System.Reflection.AssemblyProductAttribute("PetAdoption_dotnet")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PetAdoption_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
